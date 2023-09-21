bddb83c8d9d62feb23f8ee45b08b966bb4f4c41f
dimitriosg <dimitriosg2002@gmail.com>
2023-09-21 16:00:32 +0300

    add CORS to express

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.use(activityLogger); 
 
 // Middleware for CORS (if needed)
-// app.use(cors());
+app.use(cors());
 // OR
 //app.use(cors({
 //  origin: ['http://localhost:3000', 'https://myapp.com']
