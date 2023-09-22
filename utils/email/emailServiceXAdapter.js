036b91252917656f45a9e9f3d5de77e79c288311
dimitriosg <dimitriosg2002@gmail.com>
2023-09-22 14:40:46 +0300

    remove unecessary files (mailgun)

--- utils/email/emailServiceXAdapter.js
+++
@@ -1,9 +0,0 @@
-import EmailInterface from './interface.js';
-
-class EmailServiceXAdapter extends EmailInterface {
-    sendEmail(to, subject, text) {
-        // Implement email sending using EmailServiceX
-    }
-}
-
-export default new EmailServiceXAdapter();
