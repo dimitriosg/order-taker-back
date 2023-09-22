b2008d81e1474c0a9da9b24c3f81822b6756805c
dimitriosg <dimitriosg2002@gmail.com>
2023-09-22 14:33:07 +0300

    add awsSesAdapter

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -1,7 +1,8 @@
-import MailgunAdapter from './email/mailgunAdapter.js';
 // import EmailServiceXAdapter from './email/emailServiceXAdapter.js';  // Uncomment to switch to EmailServiceX
+//import MailgunAdapter from './email/mailgunAdapter.js';
+import AwsSesAdapter from "./email/awsSesAdapter";
 
-const emailAdapter = MailgunAdapter;
+const emailAdapter = AwsSesAdapter;
 // const emailAdapter = EmailServiceXAdapter;  // Uncomment to switch to EmailServiceX
 
 export function sendEmail(to, subject, text) {
