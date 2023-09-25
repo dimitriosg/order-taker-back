ea3a787efb906498672fd0213ed6e509d12aaebd
dimitriosg <dimitriosg2002@gmail.com>
2023-09-25 16:04:08 +0300

    update server.js

--- server.js
+++ server.js
@@ -49,9 +49,12 @@ const __dirname = dirname(__filename);
 app.use(express.static(join(__dirname, 'frontend/build')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'frontend/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Only use the catchall handler in production
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname + '/frontend/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
