d6e22765cdd4bf7df886b1276ac57148cb4de569
dimitriosg <dimitriosg2002@gmail.com>
2023-09-25 17:43:26 +0300

    update cors in index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 app.use(activityLogger); 
 
 // Middleware for CORS (anywhere)
-app.use(cors());
+//app.use(cors());
 
 // Middleware for authentication (if you have one globally)
 // app.use(authMiddleware);
