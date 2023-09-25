90eae918ae512304f292f8397f3131a4a6b6cfa1
dimitriosg <dimitriosg2002@gmail.com>
2023-09-25 17:00:48 +0300

    update dashAPI.js

--- routes/dashboard/dashAPI.js
+++ routes/dashboard/dashAPI.js
@@ -5,16 +5,28 @@ import dashboardController from '../../controllers/dashboard.controller.js';
 
 const router = express.Router();
 
+// --> ALL PURPOSE ROUTES <--
 // Route for fetching dashboard data
 router.get('/data', dashboardController.fetchDashboardData);
 
-// Route for fetching cashier details
+// <<< WAITER ROUTES >>>
+
+// <<< CASHIER ROUTES >>>
 router.get('/cashier/details', dashboardController.cashier.fetchCashierDetails);
+router.post('/cashier/update-cash-holding', dashboardController.cashier.updateCashHolding);
+router.post('/cashier/notify-accountant', dashboardController.cashier.notifyAccountant);
+
+// <<< ACCOUNTANT ROUTES >>>
+// Route for fetching accountant details
+router.get('/acc/details', dashboardController.acc.fetchAccDetails);
 
 // Route for updating cash holding
-router.post('/cashier/update-cash-holding', dashboardController.cashier.updateCashHolding);
+router.post('/acc/update-cash-holding', dashboardController.acc.updateCashHolding);
 
-// Route for notifying the accountant
-router.post('/cashier/notify-accountant', dashboardController.cashier.notifyAccountant);
+
+// <<< ADMIN ROUTES >>>
+
+// <<< DEVELOPER ROUTES >>>
+
 
 export default router;
