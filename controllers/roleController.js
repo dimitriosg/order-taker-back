ba620f6817e63579f32bf055eb1d80b1edd04f3d
dimitriosg <dimitriosg2002@gmail.com>
2023-09-28 14:57:40 +0300

    fix getAllRoles

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -4,22 +4,27 @@ import Role from '../models/Role.js';
 
 export const getAllRoles = async (req, res) => {
     console.log(`CONSOLE: Entered getAllRoles function`);
-
     try {
+        const roles = await Role.find();
+
         // Get the count of users for each role
-        const roles = await Role.aggregate([
+        const userCounts = await User.aggregate([
             { $group: { _id: '$role', count: { $sum: 1 } } }
         ]);
-        console.log("Role Counts:", userCounts);
+        console.log("User Counts:", userCounts);
 
-        // Optionally, get teht total of count of roles
+        // Optionally, get the total count of roles
         const totalRoles = await Role.countDocuments();
+        console.log("Total Roles:", totalRoles);
 
         res.status(200).json({
             roles,
+            userCounts,
             totalRoles
         });
-    }catch (error) {
-        res.status(400).json({ message: 'Error listing roles', error });
+    } catch (error) {
+        console.error(error);  // Log the error to the console
+        res.status(500).json({ message: error.message });
     }
 };
+
