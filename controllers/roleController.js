833c50ccb6633e412e88b7b3cfa8512ecf042718
dimitriosg <dimitriosg2002@gmail.com>
2023-09-28 15:16:07 +0300

    update getAllRoles

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -6,10 +6,17 @@ import Role from '../models/Role.js';
 
 export const getAllRoles = async (req, res) => {
     console.log(`CONSOLE: Entered getAllRoles function`);
+
     try {
         const roles = await Role.find();
-        const roleNames = roles.map(role => role.name);  // Extract the name of each role
+        console.log('Roles:', roles);  // Log the roles
 
+        if (!roles || roles.length === 0) {
+            console.log('No roles found');
+            return res.status(404).json({ message: 'No roles found' });
+        }
+
+        const roleNames = roles.map(role => role.name);  // Extract the name of each role
         res.status(200).json(roleNames);
     } catch (error) {
         console.error(error);  // Log the error to the console
