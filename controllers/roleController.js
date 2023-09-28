7f4072589fdabf2a6a2c846439f532885f00b959
dimitriosg <dimitriosg2002@gmail.com>
2023-09-28 15:10:13 +0300

    update getAllRoles

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,28 +1,16 @@
 // controllers/roleController.js
 
 import mongoose from 'mongoose';
+import User from '../models/User.js';
 import Role from '../models/Role.js';
 
 export const getAllRoles = async (req, res) => {
     console.log(`CONSOLE: Entered getAllRoles function`);
     try {
         const roles = await Role.find();
+        const roleNames = roles.map(role => role.name);  // Extract the name of each role
 
-        // Get the count of users for each role
-        const userCounts = await User.aggregate([
-            { $group: { _id: '$role', count: { $sum: 1 } } }
-        ]);
-        console.log("User Counts:", userCounts);
-
-        // Optionally, get the total count of roles
-        const totalRoles = await Role.countDocuments();
-        console.log("Total Roles:", totalRoles);
-
-        res.status(200).json({
-            roles,
-            userCounts,
-            totalRoles
-        });
+        res.status(200).json(roleNames);
     } catch (error) {
         console.error(error);  // Log the error to the console
         res.status(500).json({ message: error.message });
