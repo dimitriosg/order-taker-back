60f9a579ed31d865898d62d94eefc7126b29cd99
Dimitris Zeus <63305437+dimitriosg@users.noreply.github.com>
2023-10-05 21:44:06 +0300

    add route for categories

--- routes/menuItemsRoutes.js
+++ routes/menuItemsRoutes.js
@@ -61,4 +61,13 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/categories', async (req, res) => {
+    try {
+        const categories = await MenuItem.distinct('category');
+        res.status(200).json(categories);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch categories.' });
+    }
+});
+
 export default router;
